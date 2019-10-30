f82898d0d113d844b73e78c96be482002e49abed
SuzukiAina <32485974+SuzukiAina@users.noreply.github.com>
2019-10-30 20:09:15 +0800

    重新提交，之前傻逼版本突然运行不起来了，还没数据库

---
+++ src/main/java/rqchen/fkbbs/FkbbsApplication.java
@@ -0,0 +1,13 @@
+package rqchen.fkbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class FkbbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FkbbsApplication.class, args);
+    }
+}
---
+++ src/test/java/rqchen/fkbbs/FkbbsApplicationTests.java
@@ -0,0 +1,13 @@
+package rqchen.fkbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FkbbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
